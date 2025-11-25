dea1ba6847c482e2302fc0e88f0b26151388b038
Rian Ernesto <rianernesto9@gmail.com>
2025-11-25 10:59:20 -0300

    :wrench: config: Configuração base da aplicação para rodar funcionando

--- backend/src/services/prisma.service.ts
+++ backend/src/services/prisma.service.ts
@@ -1,4 +1,11 @@
+import { env } from "../config/env.config";
 import { PrismaClient } from "../generated/prisma/client";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
-// @ts-ignore
-export const prisma = new PrismaClient();
+const adapter = new PrismaBetterSqlite3({
+  url: env.DATABASE_URL,
+});
+
+export const prisma = new PrismaClient({
+  adapter,
+});
