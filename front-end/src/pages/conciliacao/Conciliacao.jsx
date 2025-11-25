6f41f3d5a45752872d0df07a8e196ed93d233059
vityudi <vitoryudi.artur@gmail.com>
2025-11-25 10:46:52 -0300

    :sparkles: feat: Pagina de conciliacao

---
+++ front-end/src/pages/conciliacao/Conciliacao.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function Conciliacao() {
+  return (
+    <div>
+      <h1>Conciliação</h1>
+      {/* Add your content here */}
+    </div>
+  )
+}
+
+export default Conciliacao
