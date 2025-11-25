30dc3b4dd399d85f745fd912cbb2a0a9b173051c
Samuel Pinheiro <samuelps147@gmail.com>
2025-11-25 11:16:19 -0300

    feat: implement get caixa by id route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
 import { loggerMiddleware } from "./middleware/logger.midleware";
+
 import lojasRoutes from "./routes/loja.routes";
+import caixasRoutes from "./routes/caixa.routes";
 
 const app = express();
 
@@ -10,5 +12,6 @@ app.use(express.json());
 app.use(loggerMiddleware);
 
 app.use("/lojas", lojasRoutes);
+app.use("/caixas", caixasRoutes);
 
 export default app;
