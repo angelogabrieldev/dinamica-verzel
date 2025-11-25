761d118e663f12d401aa94d248c0e4cdf6dc3fe8
vityudi <vitoryudi.artur@gmail.com>
2025-11-25 11:14:25 -0300

    :recycle: refactor: reestructure page

--- front-end/src/pages/conciliacao/Conciliacao.jsx
+++ front-end/src/pages/conciliacao/Conciliacao.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import { Container, Box, Typography } from '@mui/material'
+import { Container, Box, Typography, Grid } from '@mui/material'
+import SalesCard from '../../components/SalesCard'
 import ResumoCard from '../../components/Resumocard'
 
 function Conciliacao() {
@@ -24,18 +25,35 @@ function Conciliacao() {
         </Typography>
       </Box>
 
-      <Box sx={{ maxWidth: 500 }}>
-        <ResumoCard
-          caixaId={caixaId}
-          fetchData={false}
-          totalVendas={1500.00}
-          totalDepositos={1450.00}
-          status="ABERTO"
-          solicitacoes={[]}
-          onSolicitarEvidencia={handleSolicitarEvidencia}
-          onFinalizarConciliacao={handleFinalizarConciliacao}
-        />
-      </Box>
+      <Grid container spacing={3}>
+        {/* Sales Card */}
+        <Grid item xs={12} sm={6} md={4}>
+          <SalesCard />
+        </Grid>
+
+        {/* Deposit Card - Placeholder */}
+        <Grid item xs={12} sm={6} md={4}>
+          <Box sx={{ p: 3, border: '2px dashed', borderColor: 'grey.300', borderRadius: 2, display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: 300 }}>
+            <Typography variant="body2" color="text.secondary">
+              DepositCard (será criado)
+            </Typography>
+          </Box>
+        </Grid>
+
+        {/* Resumo Card */}
+        <Grid item xs={12} sm={6} md={4}>
+          <ResumoCard
+            caixaId={caixaId}
+            fetchData={false}
+            totalVendas={1500.00}
+            totalDepositos={1450.00}
+            status="ABERTO"
+            solicitacoes={[]}
+            onSolicitarEvidencia={handleSolicitarEvidencia}
+            onFinalizarConciliacao={handleFinalizarConciliacao}
+          />
+        </Grid>
+      </Grid>
     </Container>
   )
 }
